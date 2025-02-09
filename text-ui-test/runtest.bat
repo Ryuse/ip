6cd3e2ac4c2c34b21629d4725af385abce15507b
Ryuse <Leemuel01234@gmail.com>
2025-02-10 01:24:13 +0800

    Update `runtest.bat` and `runtest.sh`

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -8,7 +8,7 @@ if exist ACTUAL.TXT del ACTUAL.TXT
 if exist data\tasks.txt del data\tasks.txt
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\hokmah\*.java
 
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
